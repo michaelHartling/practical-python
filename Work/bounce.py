7981049f5c84fa35a8bef1b80e2df6db6e4c9858
Michael Hartling <michaelHartling@users.noreply.github.com>
2023-09-15 09:45:04 -0400

    Update bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,13 @@
 # bounce.py
 #
 # Exercise 1.5
+counter = 1
+height = 100
+bounce_back_rate = 0.6
+bounce_back_height = 0
+
+while counter <= 10:
+    bounce_back_height = height * bounce_back_rate
+    print(counter, round(bounce_back_height,4))
+    height = bounce_back_height
+    counter += 1
